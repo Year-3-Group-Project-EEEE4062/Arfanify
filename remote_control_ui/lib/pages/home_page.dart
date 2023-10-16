f4ef99fb483415507c2c5de186a754194b062537
Arfan <arfandnl@gmail.com>
2023-10-16 09:07:53 +0800

    update

--- remote_control_ui/lib/pages/home_page.dart
+++ remote_control_ui/lib/pages/home_page.dart
@@ -37,91 +37,95 @@ class homePage extends StatelessWidget {
       ),
     );
   }
-}
 
-AppBar bar() {
-  return AppBar(
-    //adjust the size of the app bar
-    toolbarHeight: 50,
-    //styling of the text in the app bar
-    title: const Text(
-      'Home',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 20,
-        fontWeight: FontWeight.w300,
+  //////////////////////////////////////////////////////////////////////////////
+  AppBar bar() {
+    return AppBar(
+      //adjust the size of the app bar
+      toolbarHeight: 50,
+      //styling of the text in the app bar
+      title: const Text(
+        'Home',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.w300,
+          height: 2.3,
+        ),
       ),
-    ),
-    //alignment of the text in the app bar
-    centerTitle: true,
-    //set background colour of AppBar
-    backgroundColor: Colors.black,
-    //adjust the bottom shape of the appbar
-    shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(5))),
-  );
-}
+      //resize the hamburger icon
+      iconTheme: IconThemeData(size: 45, color: Colors.white),
+      //alignment of the text in the app bar
+      centerTitle: true,
+      //set background colour of AppBar
+      backgroundColor: Colors.black,
+      //adjust the bottom shape of the appbar
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(5))),
+    );
+  }
 
-DrawerHeader drawer_header() {
-  return DrawerHeader(
-    decoration: BoxDecoration(
-        color: Color(0xff333333), borderRadius: BorderRadius.circular(10)),
-    //Container just acts as a filler so that Drawer Header has a child
-    //if not, Drawer header not valid
-    child: Container(
-      alignment: Alignment.topLeft,
-    ),
-  );
-}
-
-ListTile remote_control_drawer(BuildContext context) {
-  return ListTile(
-    leading: Icon(
-      Icons.settings_remote_sharp,
-      color: Colors.white,
-    ),
-    title: Text(
-      'Remote Control',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 19,
+  DrawerHeader drawer_header() {
+    return DrawerHeader(
+      decoration: BoxDecoration(
+          color: Color(0xff333333), borderRadius: BorderRadius.circular(10)),
+      //Container just acts as a filler so that Drawer Header has a child
+      //if not, Drawer header not valid
+      child: Container(
+        alignment: Alignment.topLeft,
       ),
-    ),
-    onTap: () {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => remote_control_page()));
-    },
-  );
-}
+    );
+  }
 
-ListTile autonomous_drawer() {
-  return const ListTile(
-    leading: Icon(
-      Icons.map_outlined,
-      color: Colors.white,
-    ),
-    title: Text(
-      'Autonomous',
-      style: TextStyle(
+  ListTile remote_control_drawer(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        Icons.settings_remote_sharp,
         color: Colors.white,
-        fontSize: 20,
       ),
-    ),
-  );
-}
+      title: Text(
+        'Remote Control',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 19,
+        ),
+      ),
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => remoteControlPage()));
+      },
+    );
+  }
 
-ListTile cloud_backup_drawer() {
-  return const ListTile(
-    leading: Icon(
-      Icons.cloud_done,
-      color: Colors.white,
-    ),
-    title: Text(
-      'Cloud Backup',
-      style: TextStyle(
+  ListTile autonomous_drawer() {
+    return const ListTile(
+      leading: Icon(
+        Icons.map_outlined,
         color: Colors.white,
-        fontSize: 20,
       ),
-    ),
-  );
+      title: Text(
+        'Autonomous',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+
+  ListTile cloud_backup_drawer() {
+    return const ListTile(
+      leading: Icon(
+        Icons.cloud_done,
+        color: Colors.white,
+      ),
+      title: Text(
+        'Cloud Backup',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
 }
--- remote_control_ui/lib/pages/remote_control_page.dart
+++ remote_control_ui/lib/pages/remote_control_page.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class remote_control_page extends StatelessWidget {
-  const remote_control_page({super.key});
+class remoteControlPage extends StatefulWidget {
+  @override
+  _remoteControlState createState() => _remoteControlState();
+}
+
+class _remoteControlState extends State<remoteControlPage> {
+  double _mode = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -9,99 +15,176 @@ class remote_control_page extends StatelessWidget {
       backgroundColor: Colors.black,
       appBar: bar(),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        //mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          forward_button(),
-          backwards_button(),
-          right_button(),
-          left_button(),
+          modeSetter(),
+          controlPad(),
         ],
       ),
     );
   }
-}
 
-SizedBox forward_button() {
-  return SizedBox(
-    height: 70,
-    width: 50,
-    child: FloatingActionButton(
-      backgroundColor: Colors.grey,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-      child: const Icon(
-        Icons.arrow_upward_outlined,
-        size: 40,
-      ),
-      onPressed: () {},
-    ),
-  );
-}
+  //////////////////////////////////////////////////////////////////////////////
+  Slider modeSetter() {
+    return Slider(
+      value: _mode,
+      min: 0.0,
+      max: 6.0,
+      divisions: 3,
+      activeColor: const Color(0xff545454),
+      thumbColor: const Color(0xff3a4c5a),
+      inactiveColor: Colors.white,
+      label: _getMode(_mode),
+      onChanged: (value) {
+        setState(() {
+          _mode = value;
+        });
+      },
+    );
+  }
 
-SizedBox backwards_button() {
-  return SizedBox(
-    height: 70,
-    width: 50,
-    child: FloatingActionButton(
-      backgroundColor: Colors.grey,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-      child: const Icon(
-        Icons.arrow_downward_outlined,
-        size: 40,
-      ),
-      onPressed: () {},
-    ),
-  );
-}
+  String _getMode(double _mode) {
+    String label = '';
+    if (_mode == 0.0) {
+      label = 'Stop';
+    } else if (_mode == 2.0) {
+      label = 'Low';
+    } else if (_mode == 4.0) {
+      label = 'Medium';
+    } else if (_mode == 6.0) {
+      label = 'High';
+    }
+    return label;
+  }
 
-SizedBox right_button() {
-  return SizedBox(
-    height: 50,
-    width: 70,
-    child: FloatingActionButton(
-      backgroundColor: Colors.grey,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-      child: const Icon(
-        Icons.arrow_forward_outlined,
-        size: 40,
+  Container controlPad() {
+    return Container(
+      color: Colors.black,
+      height: 190,
+      width: 190,
+      child: Column(
+        children: [
+          forward_button(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              left_button(),
+              const SizedBox(
+                width: 50,
+              ),
+              right_button(),
+            ],
+          ),
+          backwards_button(),
+        ],
       ),
-      onPressed: () {},
-    ),
-  );
-}
+    );
+  }
 
-SizedBox left_button() {
-  return SizedBox(
-    height: 50,
-    width: 70,
-    child: FloatingActionButton(
-      backgroundColor: Colors.grey,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-      child: const Icon(
-        Icons.arrow_back_outlined,
-        size: 40,
+  SizedBox forward_button() {
+    return SizedBox(
+      height: 70,
+      width: 50,
+      child: FloatingActionButton(
+        backgroundColor: const Color(0xff545454),
+        foregroundColor: Colors.black,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        child: const Icon(
+          Icons.arrow_upward_outlined,
+          size: 40,
+        ),
+        onPressed: () {
+          if (kDebugMode) {
+            debugPrint("Forward button pressed");
+          }
+        },
       ),
-      onPressed: () {},
-    ),
-  );
-}
+    );
+  }
 
-AppBar bar() {
-  return AppBar(
-    //adjust the size of the app bar
-    toolbarHeight: 50,
-    //styling of the text in the app bar
-    title: const Text(
-      'Remote Control',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 20,
-        fontWeight: FontWeight.w300,
+  SizedBox backwards_button() {
+    return SizedBox(
+      height: 70,
+      width: 50,
+      child: FloatingActionButton(
+        backgroundColor: const Color(0xff545454),
+        foregroundColor: Colors.black,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        child: const Icon(
+          Icons.arrow_downward_outlined,
+          size: 40,
+        ),
+        onPressed: () {
+          if (kDebugMode) {
+            debugPrint("Backwards button pressed");
+          }
+        },
       ),
-    ),
-    //alignment of the text in the app bar
-    centerTitle: true,
-    //set background colour of AppBar
-    backgroundColor: Colors.black,
-  );
+    );
+  }
+
+  SizedBox right_button() {
+    return SizedBox(
+      height: 50,
+      width: 70,
+      child: FloatingActionButton(
+        backgroundColor: const Color(0xff545454),
+        foregroundColor: Colors.black,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        child: const Icon(
+          Icons.arrow_forward_outlined,
+          size: 40,
+        ),
+        onPressed: () {
+          if (kDebugMode) {
+            debugPrint("Right button pressed");
+          }
+        },
+      ),
+    );
+  }
+
+  SizedBox left_button() {
+    return SizedBox(
+      height: 50,
+      width: 70,
+      child: FloatingActionButton(
+        backgroundColor: const Color(0xff545454),
+        foregroundColor: Colors.black,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        child: const Icon(
+          Icons.arrow_back_outlined,
+          size: 40,
+        ),
+        onPressed: () {
+          if (kDebugMode) {
+            debugPrint("Left button pressed");
+          }
+        },
+      ),
+    );
+  }
+
+  AppBar bar() {
+    return AppBar(
+      //adjust the size of the app bar
+      toolbarHeight: 50,
+      //styling of the text in the app bar
+      title: const Text(
+        'Remote Control',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.w300,
+          height: 1.8,
+        ),
+      ),
+      iconTheme: const IconThemeData(size: 40, color: Colors.white),
+      //alignment of the text in the app bar
+      centerTitle: true,
+      //set background colour of AppBar
+      backgroundColor: Colors.black,
+    );
+  }
 }
