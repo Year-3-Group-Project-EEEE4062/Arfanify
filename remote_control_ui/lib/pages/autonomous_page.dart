ebc82927e7a5919ed21d62a4717ec1b4283996fb
Arfan <arfandnl@gmail.com>
2023-11-23 15:08:33 +0800

    Update autonomous_page.dart

--- remote_control_ui/lib/pages/autonomous_page.dart
+++ remote_control_ui/lib/pages/autonomous_page.dart
@@ -932,6 +932,7 @@ class _AutonomousPagee extends State<AutonomousPagee> {
     return totalDistance;
   }
 
+  //straight line distance calculation based on HaverSine formula
   double getStraightLineDistance(lat1, lon1, lat2, lon2) {
     var R = 6371; // Radius of the earth in km
     var dLat = deg2rad(lat2 - lat1);
